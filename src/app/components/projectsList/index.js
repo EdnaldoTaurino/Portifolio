dbb54ed631016336718d7d906a4e045372ef5f03
EdnaldoTaurino <ednaldo.taurino5@gmail.com>
2025-05-14 17:45:38 -0300

    Implement code changes to enhance functionality and improve performance

--- src/app/components/projectsList/index.js
+++ src/app/components/projectsList/index.js
@@ -9,16 +9,16 @@ const projetosList = [
     demo: "https://jsd-telecom-next-js.vercel.app/",
   },
   {
-    name: "Nerddburger Hamburgueria Artesanal",
+    name: "Solicitação de abertura de empresas",
     description:
-      "Página de uma hamburgueria artesanal, feita para uma hamburgueria.",
-    imageUrl: "/projeto-nerdburger.png",
-    language: "Html, Css, JavaScript,",
-    url: "https://github.com/EdnaldoTaurino/nerdburger",
-    demo: "https://nerdburger.vercel.app/",
+      "Uma página profissional feita para empresa solicitar cadastro.",
+    imageUrl: "/empresasCRUD.png",
+    language: "Angular, NodeJs, Express, MongoDB",
+    url: "https://github.com/EdnaldoTaurino/Front-end-Abertura-de-Empresas",
+    demo: "https://front-end-abertura-de-empresas.vercel.app/",
   },
   {
-    name: "Front End CRUD lista de produtos",
+    name: "Tabela de produtos",
     description: "Este é uma parte do projeto o Front End do CRUD",
     imageUrl: "/projeto-crud.png",
     language:
@@ -26,14 +26,14 @@ const projetosList = [
     url: "https://github.com/EdnaldoTaurino/CRUD-front-Deploy-productList",
     demo: "https://front-deploy-product-list.vercel.app/",
   },
-  {
-    name: "Back End CRUD lista de produtos",
-    description: "Este é o Back End do projeto CRUD lista de produtos",
-    imageUrl: "/back-crud.png",
-    language: "Nest.js, Prisma, PostgreSQL",
-    url: "https://github.com/EdnaldoTaurino/CRUD-deploy-back-nestjs",
-    demo: "https://deploy-back-nestjs.vercel.app/",
-  },
+  // {
+  //   name: "Back End CRUD lista de produtos",
+  //   description: "Este é o Back End do projeto CRUD lista de produtos",
+  //   imageUrl: "/back-crud.png",
+  //   language: "Nest.js, Prisma, PostgreSQL",
+  //   url: "https://github.com/EdnaldoTaurino/CRUD-deploy-back-nestjs",
+  //   demo: "https://deploy-back-nestjs.vercel.app/",
+  // },
 ];
 
 export default projetosList;
